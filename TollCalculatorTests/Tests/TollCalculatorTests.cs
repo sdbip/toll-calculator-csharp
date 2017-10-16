bd9b80c0917ee1085eae7d07ad90c584a5785c4f
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-16 22:07:14 +0200

    Move local variable to a field

--- TollCalculatorTests/Tests/TollCalculatorTests.cs
+++ TollCalculatorTests/Tests/TollCalculatorTests.cs
@@ -7,15 +7,17 @@ namespace TollCalculatorTests
 	[TestFixture]
 	public class TollCalculatorTests
 	{
+		readonly TollCalculator calculator = new TollCalculator();
+
 		[Test, Ignore("Crashes at this time")]
-		public void NoFeeIfNoPasses() {
-			var calculator = new TollCalculator();
+		public void NoFeeIfNoPasses()
+		{
 			calculator.GetTollFee(new Car(), new DateTime[0]);
 		}
 
 		[Test]
-		public void SinglePassFee() {
-			var calculator = new TollCalculator();
+		public void SinglePassFee()
+		{
 			var earlyMorning = new DateTime(2013, 1, 2, 6, 5, 0);
 
 			var fee = calculator.GetTollFee(new Car(), new DateTime[] { earlyMorning });
