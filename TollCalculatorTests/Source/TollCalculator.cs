ff0f14ac6924c05f96f3f909b021318af81507e4
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-16 23:39:43 +0200

    Correct indentation

--- TollCalculatorTests/Source/TollCalculator.cs
+++ TollCalculatorTests/Source/TollCalculator.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 public class TollCalculator
 {
 
-    /**
+	/**
      * Calculate the total toll fee for one day
      *
      * @param vehicle - the vehicle
@@ -13,78 +13,78 @@ public class TollCalculator
      */
 
 	public int GetTollFee(VehicleType vehicle, DateTime[] dates)
-    {
-        DateTime intervalStart = dates[0];
-        int totalFee = 0;
-        foreach (DateTime date in dates)
-        {
-            int nextFee = GetTollFee(date, vehicle);
-            int tempFee = GetTollFee(intervalStart, vehicle);
+	{
+		DateTime intervalStart = dates[0];
+		int totalFee = 0;
+		foreach (DateTime date in dates)
+		{
+			int nextFee = GetTollFee(date, vehicle);
+			int tempFee = GetTollFee(intervalStart, vehicle);
 
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies/1000/60;
+			long diffInMillies = date.Millisecond - intervalStart.Millisecond;
+			long minutes = diffInMillies / 1000 / 60;
 
-            if (minutes <= 60)
-            {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
-            }
-            else
-            {
-                totalFee += nextFee;
-            }
-        }
-        if (totalFee > 60) totalFee = 60;
-        return totalFee;
-    }
+			if (minutes <= 60)
+			{
+				if (totalFee > 0) totalFee -= tempFee;
+				if (nextFee >= tempFee) tempFee = nextFee;
+				totalFee += tempFee;
+			}
+			else
+			{
+				totalFee += nextFee;
+			}
+		}
+		if (totalFee > 60) totalFee = 60;
+		return totalFee;
+	}
 
 	private bool IsTollFreeVehicle(VehicleType vehicle)
-    {
+	{
 		return vehicle != VehicleType.Car;
-    }
+	}
 
 	public int GetTollFee(DateTime date, VehicleType vehicle)
-    {
-        if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
+	{
+		if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
 
-        int hour = date.Hour;
-        int minute = date.Minute;
+		int hour = date.Hour;
+		int minute = date.Minute;
 
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-        else return 0;
-    }
+		if (hour == 6 && minute >= 0 && minute <= 29) return 8;
+		else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
+		else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
+		else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+		else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+		else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
+		else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+		else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
+		else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+		else return 0;
+	}
 
-    private Boolean IsTollFreeDate(DateTime date)
-    {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
+	private Boolean IsTollFreeDate(DateTime date)
+	{
+		int year = date.Year;
+		int month = date.Month;
+		int day = date.Day;
 
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+		if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
 
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+		if (year == 2013)
+		{
+			if (month == 1 && day == 1 ||
+				month == 3 && (day == 28 || day == 29) ||
+				month == 4 && (day == 1 || day == 30) ||
+				month == 5 && (day == 1 || day == 8 || day == 9) ||
+				month == 6 && (day == 5 || day == 6 || day == 21) ||
+				month == 7 ||
+				month == 11 && day == 1 ||
+				month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 }
