01a0f02257bec0e72b53d129cce3cb28ab3da887
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-16 22:05:43 +0200

    Add a test for a single pass fee

--- TollCalculatorTests/Tests/TollCalculatorTests.cs
+++ TollCalculatorTests/Tests/TollCalculatorTests.cs
@@ -12,5 +12,15 @@ namespace TollCalculatorTests
 			var calculator = new TollCalculator();
 			calculator.GetTollFee(new Car(), new DateTime[0]);
 		}
+
+		[Test]
+		public void SinglePassFee() {
+			var calculator = new TollCalculator();
+			var earlyMorning = new DateTime(2013, 1, 2, 6, 5, 0);
+
+			var fee = calculator.GetTollFee(new Car(), new DateTime[] { earlyMorning });
+
+			Assert.AreEqual(8, fee);
+		}
 	}
 }
