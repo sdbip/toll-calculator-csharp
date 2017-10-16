60041c71722b8e6d26b596ea26cab5482f00dc8f
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-16 21:58:49 +0200

    Begin testing the existing code

---
+++ TollCalculatorTests/Tests/TollCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using TollFeeCalculator;
+
+namespace TollCalculatorTests
+{
+	[TestFixture]
+	public class TollCalculatorTests
+	{
+		[Test, Ignore("Crashes at this time")]
+		public void NoFeeIfNoPasses() {
+			var calculator = new TollCalculator();
+			calculator.GetTollFee(new Car(), new DateTime[0]);
+		}
+	}
+}
