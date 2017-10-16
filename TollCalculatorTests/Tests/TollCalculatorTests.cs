fb8dd142dcb3984d612fb10f92d98031f49620b1
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-10-16 22:14:10 +0200

    Test several times of day

--- TollCalculatorTests/Tests/TollCalculatorTests.cs
+++ TollCalculatorTests/Tests/TollCalculatorTests.cs
@@ -15,14 +15,24 @@ namespace TollCalculatorTests
 			calculator.GetTollFee(new Car(), new DateTime[0]);
 		}
 
-		[Test]
-		public void SinglePassFee()
+		[TestCase(6, 5, 8)]
+		[TestCase(6, 35, 13)]
+		[TestCase(7, 5, 18)]
+		[TestCase(8, 5, 13)]
+		[TestCase(8, 35, 8)]
+		[TestCase(12, 5, 0)] // TODO: Bug?
+		[TestCase(15, 5, 13)]
+		[TestCase(16, 5, 18)]
+		[TestCase(17, 5, 13)]
+		[TestCase(18, 5, 8)]
+		[TestCase(18, 30, 0)]
+		public void SinglePassFee(int hour, int minute, int expectedFee)
 		{
-			var earlyMorning = new DateTime(2013, 1, 2, 6, 5, 0);
+			var earlyMorning = new DateTime(2013, 1, 2, hour, minute, 0);
 
 			var fee = calculator.GetTollFee(new Car(), new DateTime[] { earlyMorning });
 
-			Assert.AreEqual(8, fee);
+			Assert.AreEqual(expectedFee, fee);
 		}
 	}
 }
